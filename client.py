680af1587ff41147d9ffc7e64c4a4b0546645d33
AliM-1997 <aliimortada797@gmail.com>
2024-07-07 23:03:29 +0300

    read  yaml file and extract os path

--- client.py
+++ client.py
@@ -1,16 +1,17 @@
 import yaml
 import os
+
 class Client:
-    def __init__(self, config_path=None):
-        with open(config_path, 'r') as file:
-            self.reader = yaml.safe_load(file)
-        self.server_ip = self.reader.get("ServerIPAddress")
-        self.LocalIP=self.reader.get("localIPAddress")
+    def __init__(self,config_path=None) -> None:
+        with open(config_path,'r') as file:
+            self.reader=yaml.safe_load(file)
+        self.server_ip = self.reader.get("serverIP")
+        self.LocalIP=self.reader.get("localIP")
         self.connect=False
 
     def check_IP_adress(self):
-        if self.IPAdress!=None:
-            print(f"connected to server with IP: {self.IPAdress}")
+        if self.server_ip!=None:
+            print(f"connected to server with IP: {self.server_ip}")
             self.connect=True
         else:
             print("not connected to any server")
@@ -18,7 +19,8 @@ class Client:
             print(f"Local IP{self.LocalIP}")
     
 if __name__=="__main__":
-    config_name="config.yml"
+
+    config_name="config.yaml"
     parent_directory=os.getcwd()
     config_path=os.path.join(parent_directory,config_name)
     print(config_path)
